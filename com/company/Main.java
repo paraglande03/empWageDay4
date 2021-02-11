b837cde081ab8abd354701aa5def403e82571714
paraglande03 <paraglande03@gmail.com>
2021-02-11 23:45:59 +0530

    Adding calculation for month till Max reaches

--- com/company/Main.java
+++ com/company/Main.java
@@ -6,25 +6,33 @@ public class Main {
     public static final int PART_TIME=2;
     public static final int WAGE_PER_HR=20;
     public static final int MAX_WORKING_DAYS=20;
+    public static final int MAX_WORKING_HRS=100;
 
     public static void main(String[] args) {
         int dailyWage=0;
         int empHr=0;
         int salary=0;
-        int random= (int)(Math.random()*10)%2;
-        switch (random){
-            case 0:
-            System.out.println("employee is fulltime");
-            empHr=8;
-            break;
-            case 1:
-            System.out.println("employee is parttime");
-            empHr=4;
-            break;
+        int totalDays=0;
+        int totalHrs=0;
+        while (totalDays<=MAX_WORKING_DAYS && totalHrs<=MAX_WORKING_HRS ) {
+            totalDays++;
+            totalHrs++;
+            int random = (int) (Math.random() * 10) % 2;
+            switch (random) {
+                case 0:
+                    System.out.println("employee is fulltime");
+                    empHr = 8;
+                    break;
+                case 1:
+                    System.out.println("employee is parttime");
+                    empHr = 4;
+                    break;
+            }
+            dailyWage = (WAGE_PER_HR * empHr);
+            System.out.println("Daily wage is = " + dailyWage);
+            salary+=dailyWage;
         }
-        dailyWage=(WAGE_PER_HR*empHr);
-        System.out.println("Daily wage is = "+dailyWage);
-        salary=(dailyWage*MAX_WORKING_DAYS);
+
         System.out.println("Salary of the employee is: "+salary);
 
 
