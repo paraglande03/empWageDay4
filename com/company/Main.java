68c17171b23aea8d5c8cbc6160257f00fa3aa634
paraglande03 <paraglande03@gmail.com>
2021-02-11 23:24:32 +0530

    Add UC2

--- com/company/Main.java
+++ com/company/Main.java
@@ -4,17 +4,24 @@ import java.util.Random;
 public class Main {
     public static final int FULL_TIME=1;
     public static final int PART_TIME=2;
-
+    public static final int WAGE_PER_HR=20;
 
     public static void main(String[] args) {
+        int dailyWage=0;
+        int empHr=0;
         int random= (int)(Math.random()*10)%2;
-        if(random==0){
-            System.out.println("employee is present");
-
+        switch (random){
+            case 0:
+            System.out.println("employee is fulltime");
+            empHr=8;
+            break;
+            case 1:
+            System.out.println("employee is parttime");
+            empHr=4;
+            break;
         }
-        else{
-            System.out.println("employee is absent");
-        }
+        dailyWage=(WAGE_PER_HR*empHr);
+        System.out.println("Daily wage is = "+dailyWage);
 
 
     }
